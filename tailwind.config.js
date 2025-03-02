3b5f790677ba153025966af87192406c994f95eb
ItsPrakashCoder <austinprksh@gmail.com>
2025-03-02 14:17:10 +0530

    FAET:Intialize

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // This is where Tailwind will look for class names
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
